1ede163b0c633cfbc1cfd783094cf318396ea56f
kcpedrosa <kattine.costa@gmail.com>
2020-04-06 20:59:06 -0300

    Updating config for static and media files

--- gestaoClientes/settings.py
+++ gestaoClientes/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     #siga a sequencia da aula, vc passou 30 min pra criar esse app home
     #pq digitou home aqui antes
     'home',
+    'storages',
 
 ]
 
@@ -137,7 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+#desativamos pois estamos usando a amazon aws
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -152,3 +154,21 @@ LOGIN_REDIRECT_URL = 'person_list'
 STATICFILES_DIRS = [
     'statics',
 ]
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'kcpedrosa-gestao-clientes2'
+#AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+#AWS_QUERYSTRING_AUTH = False
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
